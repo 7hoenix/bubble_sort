170b708c7b80d92268920c0662f9526bc2ac61a2
philmphoenix <jpholzmann@gmail.com>
2015-05-29 10:44:41 -0600

    Cleaned up formatting

--- bubble_sort_test.rb
+++ bubble_sort_test.rb
@@ -4,13 +4,6 @@ require_relative "bubble_sort"
 
 class BubbleSortTest < Minitest::Test
 
-  # Horace
-
-
-  # the list was not sorted at that point, so we should make another pass
-  # If we reach the end of the list without encountering any "out of order pairs",
-  # then good job us, it's sorted
-
   def test_it_sorts_an_empty_list
     assert_equal [], bubble_sort([])
   end
@@ -30,34 +23,23 @@ class BubbleSortTest < Minitest::Test
   def test_swap_swaps_provided_indices
     list = [2, 1]
     swap!(list, 0, 1)
-    assert_equal [1,2], list
+    assert_equal [1, 2], list
   end
 
   def test_it_can_swap_a_later_pair
     assert_equal [1, 2, 3], bubble_sort([1, 3, 2])
-
   end
 
   def test_sorts_when_multiple_passes_are_required
-    assert_equal [1,2,3], bubble_sort([3,2,1])
+    assert_equal [1, 2, 3], bubble_sort([3, 2, 1])
   end
 
   def test_sorts_something_more_complicated
-    assert_equal [1,2,3,4,5,6], bubble_sort([4,1,2,6,5,3])
+    assert_equal [1, 2, 3, 4, 5, 6], bubble_sort([4, 1, 2, 6, 5, 3])
   end
 
   def test_sorts_long_list
-    list = (1..600).to_a.shuffle
+    list = (1..6000).to_a.shuffle
     assert_equal list.sort, bubble_sort(list)
   end
-
-  # def test_it_returns_the_correct_value
-  #   bubble = BubbleSort.new
-  #   input = [5, 3, 2, 4, 1]
-  #   assert_equal [1, 2, 3, 4, 5], bubble.sort_this(input)
-  #
-  #   # assert_equal 9, bubble.sort_this(input)
-  # end
-
-
 end

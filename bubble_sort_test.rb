c92970ef2266fc4961e8dd5b5af05eaaf456b5a8
philmphoenix <jpholzmann@gmail.com>
2015-05-29 09:16:52 -0600

    Test an empty string first

--- bubble_sort_test.rb
+++ bubble_sort_test.rb
@@ -3,17 +3,19 @@ require 'minitest/autorun'
 require_relative 'bubble_sort'
 
 class BubbleSortTest < Minitest::Test
+
   # Horace
-  def test_true
-    assert_true
-  end
 
   # Loop through our numbers, checking each pair in succession if a pair is out of order
   # we need to swap left and right elements of the pair if we make a swap, we know
   # the list was not sorted at that point, so we should make another pass
   # If we reach the end of the list without encountering any "out of order pairs",
   # then good job us, it's sorted
-  
+
+  def test_it_sorts_an_empty_list
+    assert_equal [], bubble_sort([])
+  end
+
   def test_it_returns_the_correct_value
     bubble = BubbleSort.new
     input = [5, 3, 2, 4, 1]

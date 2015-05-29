9e43ab4eb1b8132fc457639d69e714e7fd9e8edc
philmphoenix <jpholzmann@gmail.com>
2015-05-29 09:20:56 -0600

    Commented out my code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,40 +1,52 @@
-class BubbleSort
-
-  def sort_this(sequence)
-    swaps = 0
-    round = 0
-    swaps_this_round = 1
-    while swaps_this_round > 0
-      round += 1
-      x = 1
-      swaps_this_round = 0
-      puts "Round: #{round}"
-      while x <= sequence.size - 1
-        if sequence[x] < sequence[x-1]
-          in_process = sequence[x]
-          sequence[x] = sequence[x-1]
-          sequence[x-1] = in_process
-
-          swaps_this_round += 1
-
-          swaps += 1
-
-          # Uncomment the below line to change to long form.
-          #x = sequence.size
-        else
-          x += 1
-        end
-      end
-      result = sequence.to_a
-    end
-
-    puts "Final result: #{result}"
-
-    puts "Final result: #{swaps}"
-    result
-  end
+def bubble_sort(list)
 
 end
 
-bubble = BubbleSort.new
-bubble.sort_this([5, 3, 2, 4, 1])
+
+
+
+
+
+
+
+
+# class BubbleSort
+#
+#   def sort_this(sequence)
+#     swaps = 0
+#     round = 0
+#     swaps_this_round = 1
+#     while swaps_this_round > 0
+#       round += 1
+#       x = 1
+#       swaps_this_round = 0
+#       puts "Round: #{round}"
+#       while x <= sequence.size - 1
+#         if sequence[x] < sequence[x-1]
+#           in_process = sequence[x]
+#           sequence[x] = sequence[x-1]
+#           sequence[x-1] = in_process
+#
+#           swaps_this_round += 1
+#
+#           swaps += 1
+#
+#           # Uncomment the below line to change to long form.
+#           #x = sequence.size
+#         else
+#           x += 1
+#         end
+#       end
+#       result = sequence.to_a
+#     end
+#
+#     puts "Final result: #{result}"
+#
+#     puts "Final result: #{swaps}"
+#     result
+#   end
+#
+# end
+#
+# bubble = BubbleSort.new
+# bubble.sort_this([5, 3, 2, 4, 1])
--- bubble_sort_test.rb
+++ bubble_sort_test.rb
@@ -1,6 +1,6 @@
-require 'minitest'
-require 'minitest/autorun'
-require_relative 'bubble_sort'
+require "minitest"
+require "minitest/autorun"
+require_relative "bubble_sort"
 
 class BubbleSortTest < Minitest::Test
 
@@ -16,13 +16,13 @@ class BubbleSortTest < Minitest::Test
     assert_equal [], bubble_sort([])
   end
 
-  def test_it_returns_the_correct_value
-    bubble = BubbleSort.new
-    input = [5, 3, 2, 4, 1]
-    assert_equal [1, 2, 3, 4, 5], bubble.sort_this(input)
-
-    # assert_equal 9, bubble.sort_this(input)
-  end
+  # def test_it_returns_the_correct_value
+  #   bubble = BubbleSort.new
+  #   input = [5, 3, 2, 4, 1]
+  #   assert_equal [1, 2, 3, 4, 5], bubble.sort_this(input)
+  #
+  #   # assert_equal 9, bubble.sort_this(input)
+  # end
 
 
 end
